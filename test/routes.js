388f6d4006d722df2116359ce2651999dabf28c8
Liam Oehlman <liam.oehlman@sidelab.com>
2012-02-09 13:21:46 +1000

    Basic working template in place

--- test/routes.js
+++ test/routes.js
@@ -1,30 +1,50 @@
 var proxy = require('../lib/proxy.js')
   , assert = require('assert')
-  , nginx = new proxy('/test/');
+  , path = require('path')
+  , nginx = new proxy(path.join(__dirname, '/nginx'));
+
+function include(arr, item) {
+  return (arr.indexOf(item) != -1);
+}
 
 describe('Methods', function() {
   it('Should add the route the rules object', function(done) {
-    nginx.add({'localhost' : 8000}, '/test', function() {
-      assert.equal(nginx.rules['/test'][0], 'localhost:8000', 'rule does not seem to be added');
+    nginx.add(['localhost:8000'], 'test', function() {
+      var hosts = nginx.rules[0].hosts;
+      assert.equal(true, include(hosts, 'localhost:8000'), 'rule does not seem to be added');
       done();
     });
   });
 
   it('Should delete the route from the rules object', function(done) {
-    nginx.del({'localhost' : 8000}, '/test', function() {
-      assert.equal(typeof nginx.rules['/test'], 'undefined', 'rule still exists, delete failed');
+    nginx.del(['localhost:8000'], 'test', function() {
+      assert.equal(nginx.rules.length, 0, 'rule still exists, delete failed');
       done();
     });
   });
 
   it('Should only remove one route from the rules object', function(done) {
-    nginx.add({'localhost' : 8000, '127.0.0.1' : 8001}, '/test', function() {
-      assert.equal(nginx.rules['/test'][0], 'localhost:8000', 'Both rules not added')
-      assert.equal(nginx.rules['/test'][1], '127.0.0.1:8001', 'Both rules not added')    
-      nginx.del({'localhost' : 8000}, '/test', function() {
-        assert.equal(nginx.rules['/test'], '127.0.0.1:8001', 'Failure to delete rule ' + nginx.rules);
+    nginx.add(['localhost:8000', '127.0.0.1:8001'], 'test', function() {
+      var hosts = nginx.rules[0].hosts;
+      assert.equal(true, include(hosts, 'localhost:8000'), 'Both rules not added');
+      assert.equal(true, include(hosts, '127.0.0.1:8001'), 'Both rules not added'); 
+      // remove only one rule
+      nginx.del(['localhost:8000'], 'test', function() {
+        hosts = nginx.rules[0].hosts
+        assert.equal(true, include(hosts, '127.0.0.1:8001'), 'Wrong rule removed');
+        assert.equal(false, include(hosts, 'localhost:8000'), 'Rule still present');
+        // reset the routes for the next test
+        nginx.rules = [];
         done();
       });
     });
   });
+  
+  it('Should output an updated config', function(done) {
+    nginx.add(['localhost:8001', 'localhost:8002', 'localhost:8003'], 'test', function(){
+      console.log(nginx.rules);
+      nginx.update();
+      done()
+    });
+  })
 });
---
+++ test/server/server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+  , server1 = express.createServer()
+  , server2 = express.createServer()
+  , server3 = express.createServer();
+
+// set up three servers for the tests
+
+server1.get('/test', function(req, res) {
+  console.log('server1 hit')
+  res.send('server1');
+});
+
+server2.get('/test', function(req, res) {
+  console.log('server2 hit')
+  res.send('server2');
+});
+
+server3.get('/test', function(req, res) {
+  console.log('server3 hit')
+  res.send('server3');
+});
+
+server1.listen(8001);
+server2.listen(8002);
+server3.listen(8003);
